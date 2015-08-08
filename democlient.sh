737505bf374ceaeb518efb9e3200964212673caa
Kevin <luhkevin@gmail.com>
2015-08-08 00:02:00 -0700

    no delay

--- democlient.sh
+++ democlient.sh
@@ -2,11 +2,10 @@
 # repeatedly runs democlient at random intervals up to MAXSLEEP seconds
 
 MAXSLEEP=1
-DELAY=0.01
 
 while true; do
     #TZ='America/Los_Angeles' date
     node democlient.js #or nodejs democlient.js depending on environment
     #sleep $(( $RANDOM % $MAXSLEEP ))
-    sleep $(DELAY)
+    #sleep $(DELAY)
 done
