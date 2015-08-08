6b8faea005105b49762a7b19eee635c19a4e76a8
Kevin <luhkevin@gmail.com>
2015-08-08 00:20:04 -0700

    logs

--- startdemoclient.sh
+++ startdemoclient.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 nohup sudo -E ./democlient.sh | ts -i >> democlient.log2 &
+nohup sudo -E ./democlient.sh | ts -i >> democlient.log3 &
+nohup sudo -E ./democlient.sh | ts -i >> democlient.log4 &
+nohup sudo -E ./democlient.sh | ts -i >> democlient.log5 &
